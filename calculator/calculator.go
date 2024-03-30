571a1b314870079810f0344acb56fccfb594b105
Matias Rivadeneira <matiasriv66@hotmail.com>
2024-03-30 18:23:24 -0300

    prueba test

---
+++ calculator/calculator.go
@@ -0,0 +1,15 @@
+// Package calculator does simple calculations.
+package calculator
+
+// Add takes two numbers and returns the result of adding
+// them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+// Subtract toma dos números a y b, y
+// devuelve el resultado de restar b de a.
+func Subtract(a, b float64) float64 {
+    return b - a
+}
+
