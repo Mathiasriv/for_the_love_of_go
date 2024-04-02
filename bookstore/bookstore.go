78d02881e5fc617ff07923dfc881332d6fc1fde0
Mathiasriv <matiasriv66@hotmail.com>
2024-04-02 17:42:27 -0300

    terminado test de ventas, cobertura

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,6 +1,16 @@
 package bookstore
 
+import "errors"
+
 type Book struct {
 	Title, Author string
 	Copies        int
+}
+
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no quedan copias")
+	}
+	b.Copies--
+	return b, nil
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -14,3 +14,41 @@ func TestBook(t *testing.T) {
 	}
 
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+
+	b := bookstore.Book{
+		Title:  "Spark joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+
+	want := 1
+
+	result, err := bookstore.Buy(b)
+
+	got := result.Copies
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
+
+func TestBuyBookLeftCopies(t *testing.T) {
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Maria Kondo",
+		Copies: 0}
+
+	_, err := bookstore.Buy(b)
+
+	if err == nil {
+		t.Error("want error buying zero copies, got nill")
+	}
+}
